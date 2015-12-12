aa1de5803cc45639648bc64b33283f8376adb1e4
Dietmar-Franken <d.franken@umsons.de>
2015-12-12 13:27:39 +0100

    Create backupsmanagement.lang.php

---
+++ pcc/data/lang/en/acp/backupsmanagement.lang.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Backupverwaltung Sprachvariablen
+ *
+ * @author       Oliver Kleditzsch
+ * @copyright    Copyright (c) 2014, Oliver Kleditzsch
+ * @license      http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since        2.0.0-0
+ * @version      2.0.0-0
+ * @translation  Dietmar Franken
+ * @SHC Forum    http://rpi-controlcenter.de/member.php?action=profile&uid=173
+ */
+ 
+$l = array();
+
+//Allgemein
+//General
+
+//$l['acp.backupsManagement.title'] = 'Backups verwalten';
+$l['acp.backupsManagement.title'] = 'Manage backups';
+
+//Raumuebersicht
+//Room overview
+
+//$l['acp.backupsManagement.backupList.table.head.name'] = 'Name';
+$l['acp.backupsManagement.backupList.table.head.name'] = 'Name';
+//$l['acp.backupsManagement.backupList.table.head.size'] = 'Dateigröße';
+$l['acp.backupsManagement.backupList.table.head.size'] = 'Filesize';
+
+//Buttons
+//Button
+
+//$l['acp.backupsManagement.button.makeBackup'] = 'Backup erstellen';
+$l['acp.backupsManagement.button.makeBackup'] = 'Create backup';
+
+//Prompts
+//Prompts
+
+//$l['acp.backupsManagement.prompt.deleteBackup.title'] = 'Backup löschen';
+$l['acp.backupsManagement.prompt.deleteBackup.title'] = 'Delete backup';
+//$l['acp.backupsManagement.prompt.deleteBackup'] = 'Willst du das Backup wirklich löschen?';
+$l['acp.backupsManagement.prompt.deleteBackup'] = 'Are youu sure to delete the backup?';
+
+//Meldungen
+//Messages
+
+//$l['acp.backupsManagement.success.makeBackup'] =  'Das Backup wurde erfolgreich erstellt';
+$l['acp.backupsManagement.success.makeBackup'] =  'The backup has been successfully created';
+//$l['acp.backupsManagement.error.makeBackup'] =  'Das Backup konnte nicht erstellt werden';
+$l['acp.backupsManagement.error.makeBackup'] =  'The backup could not be created';
+//$l['acp.backupsManagement.success.deleteBackup'] =  'Das Backup wurde erfolgreich gelöscht';
+$l['acp.backupsManagement.success.deleteBackup'] =  'The backup has been successfully deleted';
+//$l['acp.backupsManagement.error.deleteBackup'] =  'Das Backup konnte nicht gelöscht werden';
+$l['acp.backupsManagement.error.deleteBackup'] =  'The backup could not be deleted';
+//$l['acp.backupsManagement.error.hash'] =  'Ungültiger Hash';
+$l['acp.backupsManagement.error.hash'] =  'Invalid hash';
