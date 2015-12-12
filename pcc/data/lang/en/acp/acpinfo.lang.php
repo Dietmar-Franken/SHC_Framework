802fe73ebb09f8a3082c93c4216fc0a7fbdba7e5
Dietmar-Franken <d.franken@umsons.de>
2015-12-12 11:22:30 +0100

    Create acpinfo.lang.php

---
+++ pcc/data/lang/en/acp/acpinfo.lang.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * ACP Info Sprachvariablen
+ *
+ * @author       Oliver Kleditzsch
+ * @copyright    Copyright (c) 2014, Oliver Kleditzsch
+ * @license      http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since        2.0.0-0
+ * @version      2.0.0-0
+ * @translation  Dietmar Franken
+ * @SHC Forum    http://rpi-controlcenter.de/member.php?action=profile&uid=173
+ */
+ 
+$l = array();
+
+//Titel
+//Title
+
+//$l['acp.acpinfo.title'] = 'System Informationen';
+$l['acp.acpinfo.title'] = 'System Informations';
+
+//SHC Version
+
+
+//$l['acp.acpinfo.box.pccVersion'] = 'PCC Version';
+$l['acp.acpinfo.box.pccVersion'] = 'PCC Version';
+//$l['acp.acpinfo.box.pccVersion.rwf'] = 'RWF Version';
+$l['acp.acpinfo.box.pccVersion.rwf'] = 'RWF Version';
+//$l['acp.acpinfo.box.pccVersion.pcc'] = 'PCC Version';
+$l['acp.acpinfo.box.pccVersion.pcc'] = 'PCC Version';
+//$l['acp.acpinfo.box.pccVersion.size'] = 'Speicherbedarf';
+$l['acp.acpinfo.box.pccVersion.size'] = 'Memory requirements';
+
+//Versionen
+//Versions
+
+//$l['acp.acpinfo.box.version'] = 'Versionen';
+$l['acp.acpinfo.box.version'] = 'Versions';
+//$l['acp.acpinfo.box.version.php'] = 'PHP';
+$l['acp.acpinfo.box.version.php'] = 'PHP';
+//$l['acp.acpinfo.box.version.wiringPi.unknown'] = 'unbekannt';
+$l['acp.acpinfo.box.version.wiringPi.unknown'] = 'unknown';
+//$l['acp.acpinfo.box.version.jQuery'] = 'jQuery';
+$l['acp.acpinfo.box.version.jQuery'] = 'jQuery';
+//$l['acp.acpinfo.box.version.jQueryUi'] = 'jQuery Ui';
+$l['acp.acpinfo.box.version.jQueryUi'] = 'jQuery Ui';
+//$l['acp.acpinfo.box.version.jQueryMobile'] = 'jQuery Mobile';
+$l['acp.acpinfo.box.version.jQueryMobile'] = 'jQuery Mobile';
+//$l['acp.acpinfo.box.version.mobileDetect'] = 'Mobile Detect';
+$l['acp.acpinfo.box.version.mobileDetect'] = 'Mobile Detect';
+//$l['acp.acpinfo.box.version.jQueryCookie'] = 'jQuery Cookie';
+$l['acp.acpinfo.box.version.jQueryCookie'] = 'jQuery Cookie';
+
+//Schreibrechte
+//Write access
+
+//$l['acp.acpinfo.box.writePremissions.allowed'] = 'schreibbar';
+$l['acp.acpinfo.box.writePremissions.allowed'] = 'writeable';
+//$l['acp.acpinfo.box.writePremissions.denied'] = 'nicht schreibbar';
+$l['acp.acpinfo.box.writePremissions.denied'] = 'not writeable';
+//$l['acp.acpinfo.box.writePremissions'] = 'Schreibrechte';
+$l['acp.acpinfo.box.writePremissions'] = 'Write access';
+
+//Schreibrechte
+//Write access
+
+//$l['acp.acpinfo.box.credits'] = 'Credits';
+$l['acp.acpinfo.box.credits'] = 'Credits';
+//$l['acp.acpinfo.box.credits.engineer'] = 'Entwickler';
+$l['acp.acpinfo.box.credits.engineer'] = 'Developer';
+//$l['acp.acpinfo.box.credits.projectPage'] = 'Projektseite';
+$l['acp.acpinfo.box.credits.projectPage'] = 'Project page';
+//$l['acp.acpinfo.box.credits.licence'] = 'Lizenzhinweise';
+$l['acp.acpinfo.box.credits.licence'] = 'Licence information';
+//$l['acp.acpinfo.box.credits.page'] = 'Webseite';
+$l['acp.acpinfo.box.credits.page'] = 'Website';
