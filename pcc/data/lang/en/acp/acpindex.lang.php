3a379c9af5037ab5c64dade9f9ecde20d627a40f
Dietmar-Franken <d.franken@umsons.de>
2015-12-12 11:05:56 +0100

    Create acpindex.lang.php

---
+++ pcc/data/lang/en/acp/acpindex.lang.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * ACP Info Sprachvariablen
+ *
+ * @author       Oliver Kleditzsch
+ * @copyright    Copyright (c) 2014, Oliver Kleditzsch
+ * @license      http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @since        2.0.0-0
+ * @version      2.0.0-0
+ * @translation  Dietmar Franken
+ * @SHC Forum    http://rpi-controlcenter.de/member.php?action=profile&uid=173
+ */
+ 
+$l = array();
+
+//Allgemein
+//General
+
+//$l['acpindex.text.acp'] = 'Administration';
+$l['acpindex.text.acp'] = 'Administration';
+//$l['acpindex.options'] = 'Optionen';
+$l['acpindex.options'] = 'Options';
+//$l['acpindex.sort'] = 'sortieren';
+$l['acpindex.sort'] = 'sort';
+//$l['acpindex.save'] = 'speichern';
+$l['acpindex.save'] = 'save';
+//$l['acpindex.allUsers'] = 'jede Gruppe';
+$l['acpindex.allUsers'] = 'every group';
+//$l['acpindex.reload'] = 'aktualisieren';
+$l['acpindex.reload'] = 'reload';
+//$l['acpindex.emptyList'] = 'keine Einträge';
+$l['acpindex.emptyList'] = 'no entries';
+
+//Boxen
+//Boxes
+
+//$l['acpindex.box.overview'] = 'Übersicht';
+$l['acpindex.box.overview'] = 'Overview';
+//$l['acpindex.box.acpMenue'] = 'Administrationsmenü';
+$l['acpindex.box.acpMenue'] = 'Administrationmenu';
+
+//Buttons Hauptmenue
+//Button Mainmenu
+
+//$l['acpindex.menu.group.main'] = 'Allgemein';
+$l['acpindex.menu.group.main'] = 'General';
+//$l['acpindex.menu.users'] = 'Benutzer';
+$l['acpindex.menu.users'] = 'User';
+//$l['acpindex.menu.settings'] = 'Einstellungen';
+$l['acpindex.menu.settings'] = 'Settings';
+//$l['acpindex.menu.backup'] = 'Backup';
+$l['acpindex.menu.backup'] = 'Backup';
+//$l['acpindex.menu.database'] = 'Datenbank';
+$l['acpindex.menu.database'] = 'Database';
+//$l['acpindex.menu.group.other'] = 'Sonstiges';
+$l['acpindex.menu.group.other'] = 'Others';
+//$l['acpindex.menu.info'] = 'Info';
+$l['acpindex.menu.info'] = 'Info';
